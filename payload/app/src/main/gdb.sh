25f6b149da8b2c35942fa4ffce4aa6cf2d27915b
Duron27 <duron27@gmail.com>
2024-02-14 05:40:53 -0500

    Update gdb.sh

--- payload/app/src/main/gdb.sh
+++ payload/app/src/main/gdb.sh
@@ -5,17 +5,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
 # first argument: arch of the executable used (you have to figure it out yourself!)
-ARCH=${1:-arm}
-source ../../../buildscripts/include/version.sh
+ARCH=${ARCH}
 
 # set up fake "jni" so that ndk-gdb can find a "valid" Android.mk
 rm -rf jni && mkdir jni
-echo "APP_ABI := $ABI" > jni/Android.mk
+echo "APP_ABI := ${ABI}" > jni/Android.mk
 
 rm -f gdb.exec
 echo "shell rm -rf jni" >> gdb.exec
-echo "set solib-search-path ../../../buildscripts/symbols/$ABI/" >> gdb.exec
+echo "set solib-search-path ../$ABI/" >> gdb.exec
 echo "set history save on" >> gdb.exec
 echo "set breakpoint pending on" >> gdb.exec
 
-../../../buildscripts/toolchain/ndk/ndk-gdb --attach is.xyz.omw_nightly.debug -x "gdb.exec"
+ndk-gdb --attach is.xyz.omw_nightly.debug -x "gdb.exec"
